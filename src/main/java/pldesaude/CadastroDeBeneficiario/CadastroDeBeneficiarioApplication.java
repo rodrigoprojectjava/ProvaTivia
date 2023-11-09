44b1c293cdc43ede98670cbeb3c4c63867632665
Rodrigo Oliveira <projetosrodrigojava@gmail.com>
2023-11-09 07:49:22 -0300

    Adicionado a anotacao ComponentScan para mapear os repositorios repository e controller

--- src/main/java/pldesaude/CadastroDeBeneficiario/CadastroDeBeneficiarioApplication.java
+++ src/main/java/pldesaude/CadastroDeBeneficiario/CadastroDeBeneficiarioApplication.java
@@ -2,8 +2,10 @@ package pldesaude.CadastroDeBeneficiario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"pldesaude.CadastroDeBeneficiario.controller", "pldesaude.CadastroDeBeneficiario.repository"})
 public class CadastroDeBeneficiarioApplication {
 
 	public static void main(String[] args) {
